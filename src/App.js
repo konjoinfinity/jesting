c3e093fba41c8644abdbc7b4dd91b2f69102137b
Wesley Scholl <clients@konjoweb.com>
2022-11-29 17:23:21 -0500

    testing 3 and building

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" title="header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Konjo
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Konjo/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("header renders", () => {
+  render(<App />);
+  const header = screen.getByTitle(/header/i);
+  expect(header).toBeInTheDocument();
+})
+
+test("image renders", () => {
+  render(<App />);
+  const image = screen.getByAltText(/logo/i);
+  expect(image).toBeInTheDocument();
+})
