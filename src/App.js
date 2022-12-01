9fbc419aa7a01ba4be629003914dd3ca4ae03305
Wesley Scholl <clients@konjoweb.com>
2022-12-01 14:00:53 -0500

    added hover

--- src/App.js
+++ src/App.js
@@ -4,69 +4,72 @@ import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import 'react-awesome-button/dist/styles.css';
 
+const buttonStyle = {
+  height:180, 
+  width: 180
+}
+
 function App() {
   return (
     <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Konjo
-        </p>
         <div>
-        <SocialIcon style={{height:200, width: 200}} network="facebook" />
-        <SocialIcon style={{height:200, width: 200}} network="instagram" />
-        <SocialIcon style={{height:200, width: 200}} network="discord" />
-        <SocialIcon style={{height:200, width: 200}} network="google" />
-        <SocialIcon style={{height:200, width: 200}} network="twitter" />
-        <SocialIcon style={{height:200, width: 200}} network="telegram" />
-        <SocialIcon style={{height:200, width: 200}} network="email" />
-        <SocialIcon style={{height:200, width: 200}} network="behance" />
-        <SocialIcon style={{height:200, width: 200}} network="clubhouse" />
-        <SocialIcon style={{height:200, width: 200}} network="codepen" />
-        <SocialIcon style={{height:200, width: 200}} network="dribbble" />
-        <SocialIcon style={{height:200, width: 200}} network="dropbox" />
-        <SocialIcon style={{height:200, width: 200}} network="fivehundredpix" />
-        <SocialIcon style={{height:200, width: 200}} network="flickr" />
-        <SocialIcon style={{height:200, width: 200}} network="foursquare" />
-        <SocialIcon style={{height:200, width: 200}} network="github" />
-        <SocialIcon style={{height:200, width: 200}} network="gitlab" />
-        <SocialIcon style={{height:200, width: 200}} network="google_play" />
-        <SocialIcon style={{height:200, width: 200}} network="groupme" />
-        <SocialIcon style={{height:200, width: 200}} network="itch.io" />
-        <SocialIcon style={{height:200, width: 200}} network="itunes" />
-        <SocialIcon style={{height:200, width: 200}} network="linkedin" />
-        <SocialIcon style={{height:200, width: 200}} network="linktree" />
-        <SocialIcon style={{height:200, width: 200}} network="medium" />
-        <SocialIcon style={{height:200, width: 200}} network="meetup" />
-        <SocialIcon style={{height:200, width: 200}} network="pinterest" />
-        <SocialIcon style={{height:200, width: 200}} network="reddit" />
-        <SocialIcon style={{height:200, width: 200}} network="pixiv" />
-        <SocialIcon style={{height:200, width: 200}} network="ravelry" />
-        <SocialIcon style={{height:200, width: 200}} network="rdio" />
-        <SocialIcon style={{height:200, width: 200}} network="rss" />
-        <SocialIcon style={{height:200, width: 200}} network="sharethis" />
-        <SocialIcon style={{height:200, width: 200}} network="slack" />
-        <SocialIcon style={{height:200, width: 200}} network="smugmug" />
-        <SocialIcon style={{height:200, width: 200}} network="snapchat" />
-        <SocialIcon style={{height:200, width: 200}} network="soundcloud" />
-        <SocialIcon style={{height:200, width: 200}} network="spotify" />
-        <SocialIcon style={{height:200, width: 200}} network="squarespace" />
-        <SocialIcon style={{height:200, width: 200}} network="stackoverflow" />
-        <SocialIcon style={{height:200, width: 200}} network="tiktok" />
-        <SocialIcon style={{height:200, width: 200}} network="tumblr" />
-        <SocialIcon style={{height:200, width: 200}} network="twitch" />
-        <SocialIcon style={{height:200, width: 200}} network="vevo" />
-        <SocialIcon style={{height:200, width: 200}} network="codepen" />
-        <SocialIcon style={{height:200, width: 200}} network="upwork" />
-        <SocialIcon style={{height:200, width: 200}} network="vimeo" />
-        <SocialIcon style={{height:200, width: 200}} network="vine" />
-        <SocialIcon style={{height:200, width: 200}} network="vk" />
-        <SocialIcon style={{height:200, width: 200}} network="vsco" />
-        <SocialIcon style={{height:200, width: 200}} network="wechat" />
-        <SocialIcon style={{height:200, width: 200}} network="whatsapp" />
-        <SocialIcon style={{height:200, width: 200}} network="yelp" />
-        <SocialIcon style={{height:200, width: 200}} network="youtube" />
+        <div style={{display: "flex", alignItems: "center", justifyContent:"center", flexDirection: "row", flexWrap:"wrap"}}>
+        <SocialIcon className="avatar" style={buttonStyle} network="facebook" />
+        <SocialIcon style={buttonStyle} className="avatar" network="instagram" />
+        <SocialIcon style={buttonStyle} className="avatar" network="discord" />
+        <SocialIcon style={buttonStyle} className="avatar" network="google" />
+        <SocialIcon style={buttonStyle} className="avatar" network="twitter" />
+        <SocialIcon style={buttonStyle} className="avatar" network="telegram" />
+        <SocialIcon style={buttonStyle} className="avatar" network="email" />
+        <SocialIcon style={buttonStyle} className="avatar" network="behance" />
+        <SocialIcon style={buttonStyle} className="avatar" network="clubhouse" />
+        <SocialIcon style={buttonStyle} className="avatar" network="codepen" />
+        <SocialIcon style={buttonStyle} className="avatar" network="dribbble" />
+        <SocialIcon style={buttonStyle} className="avatar" network="dropbox" />
+        <SocialIcon style={buttonStyle} className="avatar" network="fivehundredpix" />
+        <SocialIcon style={buttonStyle} className="avatar" network="flickr" />
+        <SocialIcon style={buttonStyle} className="avatar" network="foursquare" />
+        <SocialIcon style={buttonStyle} className="avatar" network="github" />
+        <SocialIcon style={buttonStyle} className="avatar" network="gitlab" />
+        <SocialIcon style={buttonStyle} className="avatar" network="google_play" />
+        <SocialIcon style={buttonStyle} className="avatar" network="groupme" />
+        <SocialIcon style={buttonStyle} className="avatar" network="itch.io" />
+        <SocialIcon style={buttonStyle} className="avatar" network="itunes" />
+        <SocialIcon style={buttonStyle} className="avatar" network="linkedin" />
+        <SocialIcon style={buttonStyle} className="avatar" network="linktree" />
+        <SocialIcon style={buttonStyle} className="avatar" network="medium" />
+        <SocialIcon style={buttonStyle} className="avatar" network="meetup" />
+        <SocialIcon style={buttonStyle} className="avatar" network="pinterest" />
+        <SocialIcon style={buttonStyle} className="avatar" network="reddit" />
+        <SocialIcon style={buttonStyle} className="avatar" network="pixiv" />
+        <SocialIcon style={buttonStyle} className="avatar" network="ravelry" />
+        <SocialIcon style={buttonStyle} className="avatar" network="rdio" />
+        <SocialIcon style={buttonStyle} className="avatar" network="rss" />
+        <SocialIcon style={buttonStyle} className="avatar" network="sharethis" />
+        <SocialIcon style={buttonStyle} className="avatar" network="slack" />
+        <SocialIcon style={buttonStyle} className="avatar" network="smugmug" />
+        <SocialIcon style={buttonStyle} className="avatar" network="snapchat" />
+        <SocialIcon style={buttonStyle} className="avatar" network="soundcloud" />
+        <SocialIcon style={buttonStyle} className="avatar" network="spotify" />
+        <SocialIcon style={buttonStyle} className="avatar" network="squarespace" />
+        <SocialIcon style={buttonStyle} className="avatar" network="stackoverflow" />
+        <SocialIcon style={buttonStyle} className="avatar" network="tiktok" />
+        <SocialIcon style={buttonStyle} className="avatar" network="tumblr" />
+        <SocialIcon style={buttonStyle} className="avatar" network="twitch" />
+        <SocialIcon style={buttonStyle} className="avatar" network="vevo" />
+        <SocialIcon style={buttonStyle} className="avatar" network="codepen" />
+        <SocialIcon style={buttonStyle} className="avatar" network="upwork" />
+        <SocialIcon style={buttonStyle} className="avatar" network="vimeo" />
+        <SocialIcon style={buttonStyle} className="avatar" network="vine" />
+        <SocialIcon style={buttonStyle} className="avatar" network="vk" />
+        <SocialIcon style={buttonStyle} className="avatar" network="vsco" />
+        <SocialIcon style={buttonStyle} className="avatar" network="wechat" />
+        <SocialIcon style={buttonStyle} className="avatar" network="whatsapp" />
+        <SocialIcon style={buttonStyle} className="avatar" network="yelp" />
+        <SocialIcon style={buttonStyle} className="avatar" network="youtube" />
         </div>
-        
+        </div>
     </div>
   );
 }
